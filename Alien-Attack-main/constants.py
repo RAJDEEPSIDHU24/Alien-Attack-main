708f5236ab81a4c636b3c88e143ce2431c512282
RAJDEEPSIDHU24 <rajdeepsidhu642@gmail.com>
2024-11-26 19:42:16 +0530

    Initial commit

---
+++ Alien-Attack-main/constants.py
@@ -0,0 +1,16 @@
+
+
+# game window dimensions
+WIDTH = 800
+HEIGHT = 600
+# number of starts to display
+NUMSTARS = 50
+# number of player lives
+START_LIVES = 3
+# Game states
+PAUSED = 0
+INPLAY = 1
+GAME_OVER = 2
+# Short Pause in gae
+DELAY = 60
+
---
+++ Alien-Attack-main/sprite_list.py
@@ -0,0 +1,24 @@
+
+
+class SpriteList():
+    def __init__(self):
+        self.list = []
+
+    def add(self, item):
+        self.list.append(item)
+
+
+    def draw(self,screen):
+        for item in self.list:
+            item.draw(screen)
+
+    def number(self):
+        return len(self.list)
+
+    def clear_done(self):
+        for item in self.list:
+            if item.done:
+                self.list.remove(item)
+
+    def clear_all(self):
+        self.list.clear()
